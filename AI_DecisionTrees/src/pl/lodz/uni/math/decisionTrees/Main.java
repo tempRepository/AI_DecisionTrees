c50f6b618745236d8aa7dc3b05382b226e0c3747
Filip Jerzyna <filipjerzyna@gmail.com>
2013-11-02 13:58:47 +0100

    Test

--- AI_DecisionTrees/src/pl/lodz/uni/math/decisionTrees/Main.java
+++ AI_DecisionTrees/src/pl/lodz/uni/math/decisionTrees/Main.java
@@ -32,26 +32,30 @@ public class Main {
         trainingSet.add(new Example(false, false, false, false, Guests.NONE, Price.CHEAP , false, false, Type.THAI, WaitEstimate.TO10, false));
         trainingSet.add(new Example(true, true, true, true,  Guests.FULL, Price.CHEAP, false, false, Type.BURGER, WaitEstimate.TO60, true));
 
-        ArrayList<Attribute> attributes=new ArrayList<>();
-/*        trainingSet.add(new Example(false, true, null, null, null, null, null, null, null, null, false));
-        trainingSet.add(new Example(true, false, null, null, null, null, null, null, null, null, false));
-        trainingSet.add(new Example(true, true, null, null, null, null, null, null, null, null, true));
-        trainingSet.add(new Example(false, false, null, null, null, null, null, null, null, null, false));
+        ArrayList<Attribute> attributes = new ArrayList<>();
+        /*
+         * trainingSet.add(new Example(false, true, null, null, null, null,
+         * null, null, null, null, false)); trainingSet.add(new Example(true,
+         * false, null, null, null, null, null, null, null, null, false));
+         * trainingSet.add(new Example(true, true, null, null, null, null, null,
+         * null, null, null, true)); trainingSet.add(new Example(false, false,
+         * null, null, null, null, null, null, null, null, false));
+         * 
+         * attributes.add(new Alternate()); attributes.add(new Bar());
+         */
 
-       attributes.add(new Alternate());
-       attributes.add(new Bar());*/
-       
-       attributes.add(new Estimate());
-       attributes.add(new FriOrSat());
-       attributes.add(new GuestsAttribute());
-       attributes.add(new Hungry());
-       attributes.add(new PriceAttribute());
-       attributes.add(new Raining());
-       attributes.add(new Reservation());
-       attributes.add(new TypeAttribute());
+        attributes.add(new Estimate());
+        attributes.add(new FriOrSat());
+        attributes.add(new GuestsAttribute());
+        attributes.add(new Hungry());
+        attributes.add(new PriceAttribute());
+        attributes.add(new Raining());
+        attributes.add(new Reservation());
+        attributes.add(new TypeAttribute());
 
-       Tree.decisionTreeLearning(trainingSet, attributes, new ArrayList<Example>()).toString();
-
+        Tree.decisionTreeLearning(trainingSet, attributes,
+                new ArrayList<Example>()).toString();
+        System.out.println("Nowy main");
 
     }
 
